f61e34327131d6c8016d3b04f55b5919d09fb2dd
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2021-10-21 01:08:48 +0300

    update integration test

--- src/commonTest/kotlin/by/cyberpartisan/psms/PSmsEncryptorIntegrationTest.kt
+++ src/commonTest/kotlin/by/cyberpartisan/psms/PSmsEncryptorIntegrationTest.kt
@@ -1,33 +1,42 @@
 package by.cyberpartisan.psms
 
+import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class PSmsEncryptorIntegrationTest {
-    private fun testEncodeDecode(str: String, keySrc: String) {
+    private fun testEncodeDecode(str: String, key: ByteArray) {
+        testEncodeDecode(Message(str), key)
+    }
+
+    private fun testEncodeDecode(message: Message, key: ByteArray) {
         val pSmsEncryptor = PSmsEncryptor()
-        val key = md5(keySrc.encodeToByteArray())
-        val encoded = pSmsEncryptor.encode(Message(str), key, 0)
+        val encoded = pSmsEncryptor.encode(message, key, 0)
         assertTrue(pSmsEncryptor.isEncrypted(encoded, key), "String must be encrypted.")
         val decoded = pSmsEncryptor.decode(encoded, key, 0)
-        assertEquals(Message(str), decoded, "Encoded and decoded strings are different.")
+        assertEquals(message, decoded, "Encoded and decoded messages are different.")
         val tryDecoded = pSmsEncryptor.tryDecode(encoded, key)
-        assertEquals(Message(str), tryDecoded, "Encoded and decoded by 'tryDecode' strings are different.")
+        assertEquals(message, tryDecoded, "Encoded and decoded by 'tryDecode' messages are different.")
     }
 
     @Test
     fun testEncodeDecodeEmpty() {
-        testEncodeDecode("", "key")
+        testEncodeDecode("", Random.nextBytes(16))
     }
 
     @Test
     fun testEncodeDecodeSingleChar() {
-        testEncodeDecode("a", "key")
+        testEncodeDecode("a", Random.nextBytes(24))
     }
 
     @Test
     fun testEncodeDecodeLong() {
-        testEncodeDecode("1234567890123456789012345678901234567890", "key")
+        testEncodeDecode("1234567890123456789012345678901234567890", Random.nextBytes(24))
+    }
+
+    @Test
+    fun testEncodeDecodeChannel() {
+        testEncodeDecode(Message("1234567890123456789012345678901234567890", 124), Random.nextBytes(16))
     }
 }
